99cf315ac71e90878d93680d8120b1dd6f21a106
Alister Maguire <maguire7@llnl.gov>
2018-08-22 11:46:04 -0700

    removing visit log

--- visitlog.py
+++
@@ -1,5 +0,0 @@
-# Visit 3.0.0b log file
-ScriptVersion = "3.0.0b"
-if ScriptVersion != Version():
-    print "This script is for VisIt %s. It may not work with version %s" % (ScriptVersion, Version())
-visit.ShowAllWindows()
